379d7088d3359ec01ad168454df708459443b9d9
juacjm <dv@MacBook-Air-de-juan.local>
2025-06-07 13:56:17 -0600

    update name del canal

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ server = "irc.chat.twitch.tv"
 port = 6667
 nickname = "xxjuancjmxx"
 token = "oauth:j1ikoit1zvcdool9iocxhz9uyhi5vh"
-canal = "#Jairo_Panda"  # Siempre con #
+canal = "#elcrissenpaii"  # Siempre con #
 
 # Crear conexión IRC
 reactor = irc.client.Reactor()
@@ -18,19 +18,19 @@ def enviar_mensaje(conn):
         conn.privmsg(canal, mensaje)
         print(f"Mensaje enviado: {mensaje}")
         time.sleep(400)  # 50 segundos
-        msj1 = "que juego es?"
+        msj1 = "que anime es?"
         conn.privmsg(canal, msj1)
         print(f"Mensaje enviado: {msj1}")
         time.sleep(400)
-        msj2 = "lo estas haciendo muy bien amig@ sigue así!!"
+        msj2 = "Esta interesante, es la primaera vez que lo veo!!"
         conn.privmsg(canal, msj2)
         print(f"Mensaje enviado: {msj2}")
         time.sleep(400)
-        msj3 = "buena partida, GG"
+        msj3 = "buen anime!, GG"
         conn.privmsg(canal, msj3)
         print(f"Mensaje enviado: {msj3}")
         time.sleep(400)
-        msj3 = "la estas flipando, sigue así"
+        msj3 = "la estas flipando tio, sigue así"
         conn.privmsg(canal, msj3)
         print(f"Mensaje enviado: {msj3}")
         time.sleep(1800)  # 1800 segundos = 30 minutos
